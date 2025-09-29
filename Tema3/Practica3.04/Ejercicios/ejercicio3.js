1f8ab0a7bca868fcaaba4ff558bdfb97d6307da6
javisr34 <javiersanchezsolera.alu@iespacomolla.es>
2025-09-29 16:21:04 +0200

    Practica3.04 terminada

--- Tema3/Practica3.04/Ejercicios/ejercicio3.js
+++ Tema3/Practica3.04/Ejercicios/ejercicio3.js
@@ -1,4 +1,5 @@
-const usuarios = [
+"use strict";
+export const usuarios = [
   {
     nombre: "Luis",
     preferencias: { tema: "oscuro", idioma: "español", edad: 25 },
@@ -91,3 +92,56 @@ const usuarios = [
     },
   },
 ];
+
+
+export const insertarUsuario = (nuevoUsuario) => {
+  return [...usuarios,nuevoUsuario]; //De esta forma crea una copia de usuarios sin modificar el original.
+}
+
+export const mayoresDeEdad = (usuarios) => {
+  return usuarios.filter(usuario => usuario.preferencias.edad>=18);
+}
+
+export const filtrarCorreoYahoo = (usuarios) => {
+  return usuarios.filter(usuario => usuario.contacto.correoelectronico.includes("@yahoo"));
+
+}
+
+export const filtrarTemaClaroMayorEdadYEspañoles = (usuarios) => {
+  //Convierto las cadenas de texto en minúsculas por si el usuario pone Claro, España o EsPaÑa.
+  return usuarios.filter(usuario => usuario.preferencias.tema.toLowerCase() === "claro" 
+  && usuario.preferencias.edad>=18 
+  && usuario.contacto.direccion.pais.toLowerCase() === "españa");
+}
+
+export const filtrarPorFaltaDatos = (usuarios) => {
+  return usuarios.filter(usuario => {
+    //Creo un array con las propiedades de cada objeto
+    let valores = [usuario.nombre, 
+      ...Object.values(usuario.preferencias ),//Con Object.values() convierto los valores de los objetos en un array.
+      ...Object.values(usuario.contacto ),
+      ...Object.values(usuario.contacto?.direccion)];//Con el encadenamiento opcional si contacto no existe devuelve undefined.
+    return valores.some(valor => valor === "" || valor === null);//Si algun valor del array es nulo o cadena vacía dice true y devuelve el usuario
+  });
+}
+
+export const añadirApellidos = (usuarios) => {
+  return usuarios.map(usuario => ({
+    ...usuario, //Copia todo el objet usuario 
+    apellidos: "No indicado" //Añade la propiedad apellidos con "No indicado" como defecto.
+  }));
+}
+
+export const añadirCodigoADireccion = (usuarios) => {
+  //En este caso tenemos que ir copiando parte por parte del objeto ya que tenemos que añadir codigo en el objeto dirección.
+  return usuarios.map(usuario => ({
+    ...usuario,
+    contacto: {
+      ...usuario.contacto,
+      direccion: {
+        ...usuario.contacto.direccion,
+        codigo: "00000"
+      }
+    }
+  }));
+}
--- Tema3/Practica3.04/main.js
+++ Tema3/Practica3.04/main.js
@@ -9,6 +9,7 @@ console.log(ordenarAlfabeticamenteInvero(array));
 
 console.log(crearArrayObjeto(array));
 
+
 //ejercicio2
 import { crearArrayAleatorio,filtrarMayorQueCinco } from "./Ejercicios/ejercicio2.js";
 let array1=crearArrayAleatorio();
@@ -16,3 +17,49 @@ let array2=crearArrayAleatorio();
 let array3=crearArrayAleatorio();
 
 console.log(filtrarMayorQueCinco(array1,array2,array3));
+
+
+//ejercicio3
+import {insertarUsuario } from "./Ejercicios/ejercicio3.js";
+//Insertar usuario
+let nuevoUsuario = {
+    nombre: "Javier",
+    preferencias: { tema: "claro", idioma: "español", edad: 19 },
+    contacto: {
+      direccion: {
+        calle: "Calle falsa, 433",
+        localidad: "Petrer",
+        pais: "España",
+      },
+      correoelectronico: "holaadios@gmail.com",
+      telefono: "111111111",
+    }
+};
+let actualizacionUsuarios = insertarUsuario(nuevoUsuario);
+console.log(actualizacionUsuarios);
+
+//Filtrar por +18
+import { mayoresDeEdad } from "./Ejercicios/ejercicio3.js";
+console.log(mayoresDeEdad(actualizacionUsuarios));//Filtro +18 de todos los usuarios, los que he añadido yo también.
+
+//Filtrar por correo yahoo
+import { filtrarCorreoYahoo } from "./Ejercicios/ejercicio3.js";
+console.log(filtrarCorreoYahoo(actualizacionUsuarios));
+
+//Filtrar por tema claro, mayores de edad y país de españa
+import { filtrarTemaClaroMayorEdadYEspañoles } from "./Ejercicios/ejercicio3.js";
+console.log(filtrarTemaClaroMayorEdadYEspañoles(actualizacionUsuarios));
+
+//Filtrar si falta algún dato
+import { filtrarPorFaltaDatos } from "./Ejercicios/ejercicio3.js";
+console.log(filtrarPorFaltaDatos(actualizacionUsuarios));
+
+//Añadir apellidos. Hago un ejemplo de añadir un apellido al nuevo usuario.
+import { añadirApellidos } from "./Ejercicios/ejercicio3.js";
+let usuariosConApellidos=añadirApellidos(actualizacionUsuarios);
+console.log(usuariosConApellidos);
+
+//Añadir código a dirección
+import { añadirCodigoADireccion } from "./Ejercicios/ejercicio3.js";
+let usuariosConApellidosYCodigo= añadirCodigoADireccion(usuariosConApellidos);//Creo una variable con el código y los apellidos, todo junto.
+console.log(usuariosConApellidosYCodigo);
